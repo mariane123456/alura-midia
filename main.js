dce1ed0ec0a2930d0f917e85f1572bbad778d4d9
mariane123456 <106816483+mariane123456@users.noreply.github.com>
2023-10-19 11:11:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,23 @@ const listaDeteclas = document.querySelectorAll('.tecla');
 for(let contador< listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
-    cont instrumento = tecla.classlist[1];
+    const instrumento = tecla.classlist[1];
     const idAudio = '#som_${instrumento}'; //template tring
-}
 
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        
+        if (evento.code === 'space' || evento.code === 'enter') {
+            tecla.classlist.add('ativa');
+        }
+
+    }
+        
+    tecla.onkeyup = function () {
+        tecla.classlist.remove('ativa');
+    }
+
+}
